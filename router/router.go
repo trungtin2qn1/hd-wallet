392e9c1a0ba7744c8c11ca7cae868937020f5cd4
Tin <trungtin2qn1@gmail.com>
2020-04-30 13:05:46 +0700

    Add router

--- router/router.go
+++ router/router.go
@@ -10,6 +10,7 @@ import (
 func InitRouter() {
 	router := gin.Default()
 
+	router.POST("/v1/wallet", controllers.AddNewWalletV1)
 	router.POST("/wallet", controllers.AddNewWallet)
 	router.POST("/retrieve-address", controllers.RetrieveAddress)
 	router.POST("/key", controllers.GenerateMasterKeys)
